07cd4bc8a1c3124be5c61988a32f1a512a92f009
satriapamudjii <163331188+satriapamudjii@users.noreply.github.com>
2024-06-25 00:02:58 +0200

    change names

--- tests/PropertyRegistryTests.sol
+++ tests/PropertyRegistryTests.sol
@@ -7,47 +7,47 @@ import "../contracts/PropertyRegistry.sol";
 contract TestPropertyRegistry {
     PropertyRegistry registry = PropertyRegistry(DeployedAddresses.PropertyRegistry());
 
-    function testInitialState() public {
-        uint expectedInitialCount = 0;
-        Assert.equal(registry.getPropertyCount(), expectedInitialCount, "Initial property count should be 0.");
+    function testInitialStateOfRegistry() public {
+        uint expectedInitialPropertyCount = 0;
+        Assert.equal(registry.getTotalPropertyCount(), expectedInitialPropertyCount, "Initial property count should be 0.");
     }
 
-    function testPropertyRegistration() public {
-        address propertyOwner = address(this);
-        string memory propertyDetails = "Property 1";
+    function testSuccessfulPropertyRegistration() public {
+        address registeringOwner = address(this);
+        string memory newPropertyDetails = "Property 1";
 
-        registry.registerProperty(propertyDetails);
+        registry.addNewProperty(newPropertyDetails);
 
-        uint expectedPropertyCount = 1;
-        Assert.equal(registry.getPropertyCount(), expectedPropertyCount, "There should be 1 property registered.");
+        uint expectedRegisteredPropertyCount = 1;
+        Assert.equal(registry.getTotalPropertyCount(), expectedRegisteredPropertyCount, "There should be 1 property registered.");
 
-        (address owner, string memory details) = registry.getProperty(expectedPropertyCount);
-        Assert.equal(owner, propertyOwner, "The owner of the first property should match.");
-        Assert.equal(details, propertyDetails, "The details of the first property should match.");
+        (address registeredOwner, string memory registeredDetails) = registry.getPropertyDetailsById(expectedRegisteredPropertyCount);
+        Assert.equal(registeredOwner, registeringOwner, "The owner of the first property should match.");
+        Assert.equal(registeredDetails, newPropertyDetails, "The details of the first property should match.");
     }
 
-    function testPropertyTransfer() public {
-        address newOwner = address(0x123);
-        uint propertyId = 1; 
+    function testPropertyOwnershipTransfer() public {
+        address recipientOwner = address(0x123);
+        uint propertyIdToTransfer = 1; 
 
-        registry.transferProperty(newOwner, propertyId);
+        registry.changePropertyOwner(recipientOwner, propertyIdToTransfer);
 
-        (address owner, ) = registry.getProperty(propertyId);
-        Assert.equal(owner, newOwner, "The new owner should match.");
+        (address currentOwner, ) = registry.getPropertyDetailsById(propertyIdToTransfer);
+        Assert.equal(currentOwner, recipientOwner, "The new owner should match.");
     }
 
-    function testQueryNonexistentProperty() public {
-        uint nonExistentPropertyId = 9999; 
+    function testUnregisteredPropertyQuery() public {
+        uint queryNonexistentPropertyId = 9999; 
 
-        (address owner, ) = registry.getProperty(nonExistentPropertyId);
+        (address ownerOfUnregistered, ) = registry.getPropertyDetailsById(queryNonexistentPropertyId);
 
-        address expectedOwner = address(0);
-        Assert.equal(owner, expectedOwner, "The owner of a non-existent property should be the 0 address.");
+        address expectedUnregisteredOwner = address(0);
+        Assert.equal(ownerOfUnregistered, expectedUnregisteredOwner, "The owner of a non-existent property should be the 0 address.");
     }
 
-    function testRegistrationFailure() public {
-        bool result = true; 
-        bool expected = false;
-        Assert.equal(result, expected, "Expected registration to fail under specific circumstances.");
+    function testExpectFailureOnInvalidRegistration() public {
+        bool registrationOutcome = true; 
+        bool expectedOutcome = false;
+        Assert.equal(registrationOutcome, expectedOutcome, "Expected registration to fail under specific circumstances.");
     }
 }
